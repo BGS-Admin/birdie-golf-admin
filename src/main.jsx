af722cc8856ed3a648267973a72036b8f8634072
Daniel Duran <danielduran@birdiegolfstudios.com>
2026-06-12 20:22:25 -0400

    refactor: rename App.jsx to AdminApp.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AdminApp from './App.jsx'
+import AdminApp from './AdminApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
